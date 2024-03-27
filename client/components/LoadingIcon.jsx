66b4942d008d335e361debe8704c756fbb412389
Kwongster <bryant.j.kwong@gmail.com>
2024-03-27 10:24:22 -0700

    Update text transition animation for LoadingIcon component

--- client/components/LoadingIcon.jsx
+++ client/components/LoadingIcon.jsx
@@ -1,7 +1,12 @@
 import React, { useState, useEffect } from "react";
 import { motion } from "framer-motion";
 
-const textArray = ["Text 1", "Text 2", "Text 3", "And so on..."]; // Replace with your text
+const textArray = [
+  "Gathering the ingredients...",
+  "Measuring just the right amount...",
+  "Mixing with care...",
+  "Finalizing the recipe...",
+];
 
 const TextTransition = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
@@ -9,7 +14,7 @@ const TextTransition = () => {
   useEffect(() => {
     const intervalId = setInterval(() => {
       setCurrentIndex((prevIndex) => (prevIndex + 1) % textArray.length);
-    }, 2000); // Update index every 2 seconds
+    }, 4000);
 
     return () => clearInterval(intervalId);
   }, []);
@@ -18,8 +23,8 @@ const TextTransition = () => {
     <div className="flex justify-center items-center">
       <motion.div
         key={currentIndex} // Key for performance
-        animate={{ opacity: [0, 1, 0] }} // Animate opacity with keyframes
-        transition={{ duration: 1, repeat: Infinity, repeatType: "reverse" }} // Control animation duration and repetition
+        animate={{ opacity: [0, 2, 0] }} // Animate opacity with keyframes
+        transition={{ duration: 2, repeat: Infinity, repeatType: "reverse" }} // Control animation duration and repetition
       >
         {textArray[currentIndex]}
       </motion.div>
