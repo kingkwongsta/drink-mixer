c570f55dcd12e1dbe0098fe0710d4632b57bff11
Kwongster <bryant.j.kwong@gmail.com>
2024-04-29 11:57:40 -0700

    Add Supabase integration for fetching test data

---
+++ backend/db/main.py
@@ -0,0 +1,12 @@
+
+import os
+from supabase import create_client, Client
+
+url: str = os.environ.get("SUPABASE_URL")
+key: str = os.environ.get("SUPABASE_KEY")
+supabase: Client = create_client(url, key)
+
+response = supabase.table('test').select("*").execute()
+
+
+print(response)
