77071822458cfaa68dc5363c1f2ffbd187b0dec5
Kwongster <bryant.j.kwong@gmail.com>
2024-05-01 13:58:02 -0700

    Update drink model and API call for recipe generation

--- backend/generate_recipe.py
+++ backend/generate_recipe.py
@@ -1,28 +1,29 @@
 from octoai.client import Client
 from octoai.chat import TextModel, ChatCompletionResponseFormat
-from pydantic import BaseModel, Field
-from typing import List
+from models import Drink
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
-client = Client()
+octoai_api_token = os.getenv("OCTOAI_API_TOKEN")
 
-class Car(BaseModel):
-    color: str
-    maker: str
+client = Client(token=octoai_api_token)
 
 completion = client.chat.completions.create(
-    model=TextModel.MISTRAL_7B_INSTRUCT,
     messages=[
-        {"role": "system", "content": "You are a helpful assistant."},
-        {"role": "user", "content": "the car was black and it was a toyota camry."},
+        {"role": "system", "content": "You are an expert mixologist"},
+        {"role": "user", "content": f"Create a unique creative advance cocktail recipe based on the following user preferences of {liquor}, {flavor}, {mood}. Do not include {flavor}, {liquor}, or {mood} in the recipe name."},
     ],
-    max_tokens=512,
+    model="meta-llama-3-8b-instruct",
+    max_tokens=2000,
     presence_penalty=0,
-    temperature=0.1,
+    temperature=0.8,
     top_p=0.9,
     response_format=ChatCompletionResponseFormat(
         type="json_object",
-        schema=Car.model_json_schema(),
+        schema=Drink.model_json_schema(),
     ),
 )
 
 print(completion.choices[0].message.content)
+
