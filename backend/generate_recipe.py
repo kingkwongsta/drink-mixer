2e62b8f59743de95a4976a639b6bffae563a2029
Kwongster <bryant.j.kwong@gmail.com>
2024-05-01 14:13:09 -0700

    Generate drink recipe based on user preferences

--- backend/generate_recipe.py
+++ backend/generate_recipe.py
@@ -1,29 +1,33 @@
 from octoai.client import Client
 from octoai.chat import TextModel, ChatCompletionResponseFormat
-from models import Drink
+from models import DrinkRecipe
 import os
 from dotenv import load_dotenv
-load_dotenv()
 
-octoai_api_token = os.getenv("OCTOAI_API_TOKEN")
+def create_recipe(liquor, flavor, mood):
+    load_dotenv()
 
-client = Client(token=octoai_api_token)
+    octoai_api_token = os.getenv("OCTOAI_API_TOKEN")
 
-completion = client.chat.completions.create(
-    messages=[
-        {"role": "system", "content": "You are an expert mixologist"},
-        {"role": "user", "content": f"Create a unique creative advance cocktail recipe based on the following user preferences of {liquor}, {flavor}, {mood}. Do not include {flavor}, {liquor}, or {mood} in the recipe name."},
-    ],
-    model="meta-llama-3-8b-instruct",
-    max_tokens=2000,
-    presence_penalty=0,
-    temperature=0.8,
-    top_p=0.9,
-    response_format=ChatCompletionResponseFormat(
-        type="json_object",
-        schema=Drink.model_json_schema(),
-    ),
-)
+    client = Client(token=octoai_api_token)
 
-print(completion.choices[0].message.content)
+    completion = client.chat.completions.create(
+        messages=[
+            {"role": "system", "content": "You are an expert mixologist"},
+            {"role": "user", "content": f"Create a unique creative advance cocktail recipe based on the following user preferences of {liquor}, {flavor}, {mood}. Do not include {flavor}, {liquor}, or {mood} in the recipe name."},
+        ],
+        model=TextModel.MISTRAL_7B_INSTRUCT,
+        max_tokens=2000,
+        presence_penalty=0,
+        temperature=0.8,
+        top_p=0.9,
+        response_format=ChatCompletionResponseFormat(
+            type="json_object",
+            schema=DrinkRecipe.model_json_schema(),
+        ),
+    )
 
+    print("********** drink recipe successfully genereated **********")
+    return completion.choices[0].message.content
+
+# create_recipe("vodka","sweet","happy")
