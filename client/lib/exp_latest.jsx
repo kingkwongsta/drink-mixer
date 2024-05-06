29688b16e4a439065ae77b4f016a9c3fdf17a865
Kwongster <bryant.j.kwong@gmail.com>
2024-05-05 18:40:13 -0700

    Refactor fetching latest recipes and store in state

--- client/lib/exp_latest.jsx
+++ client/lib/exp_latest.jsx
@@ -1,41 +1,39 @@
 // EXPERIMENT COMPONENT TO GET LATEST RECIPES
 "use client";
+import { useEffect } from "react";
 import userStore from "./userStore";
 
 export default function StoreData() {
-  const { userFlavor, userLiquor, userMood, drinkRecipe, drinkImage } =
-    userStore();
-
-  const data = {
-    user_flavor: userFlavor,
-    user_mood: userMood,
-    user_liquor: userLiquor,
-    drink_recipe: drinkRecipe,
-    drink_image: drinkImage,
-  };
-
-  const handleSubmit = async () => {
+  const { storeRecipes, setStoreRecipes } = userStore();
+  const getLatest = async () => {
     try {
-      const response = await fetch("http://127.0.0.1:8000/add_recipe", {
-        method: "POST",
+      const response = await fetch("http://127.0.0.1:8000/latest_recipes", {
+        method: "GET",
         headers: {
           "Content-Type": "application/json",
         },
         body: JSON.stringify(data),
       });
       const data2 = await response.json();
-      console.log(data2);
+      setStoreRecipes(data2);
     } catch (error) {
       console.error(error);
     }
   };
+  useEffect(() => {
+    getLatest();
+  }, []);
+
+  const handleClick = async () => {
+    console.log(storeRecipes);
+  };
 
   return (
     <button
-      onClick={handleSubmit}
+      onClick={handleClick}
       className="border-2 border-cyan-300 m-5 p-4 text-3xl"
     >
-      Store the Data
+      Get Me Latest
     </button>
   );
 }
