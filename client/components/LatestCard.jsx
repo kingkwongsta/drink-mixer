f1d611da2baa9e3c533855136ab9c4c6b60ff43c
Kwongster <bryant.j.kwong@gmail.com>
2024-05-06 20:14:36 -0700

    Refactor card display layout and description handling

--- client/components/LatestCard.jsx
+++ client/components/LatestCard.jsx
@@ -10,19 +10,27 @@ import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function LatestCard(drinkRecipe, drinkImage) {
+  const trimmedDescription = drinkRecipe.description
+    ? drinkRecipe.description.split(" ").slice(0, 15).join(" ") +
+      (drinkRecipe.description.split(" ").length > 15 ? "..." : "")
+    : "";
+
   return (
-    <Card className="w-full max-w-2xl">
+    // <Card className="max-md:w-full max-md:max-w-2xl border-2 border-solid border-gray-800">
+    <Card className="border-2 border-solid border-gray-800 sm:w-full">
       <CardHeader className="px-6 pt-6 pb-4 text-center">
-        <CardTitle className="text-3xl font-bold">{drinkRecipe.name}</CardTitle>
-        <CardDescription>{drinkRecipe.description}</CardDescription>
+        <CardTitle className="text-xl font-bold">{drinkRecipe.name}</CardTitle>
+        <CardDescription>{trimmedDescription}</CardDescription>
       </CardHeader>
-      <Image
-        alt={drinkRecipe.name}
-        className="object-cover my-2"
-        height="600"
-        src={drinkImage}
-        width="1200"
-      />
+      <div className="sm-max-w-[200px]">
+        <Image
+          alt={drinkRecipe.name}
+          className="my-2 bg-cover max-w-full"
+          height="600"
+          src={drinkImage}
+          width="1200"
+        />
+      </div>
     </Card>
   );
 }
