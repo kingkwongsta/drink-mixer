f1c4fca8fc5a8eca02382a184bcbdaa5302c8732
Kwongster <bryant.j.kwong@gmail.com>
2024-05-06 19:30:36 -0700

    Add LatestRecipes component with RecipeCard rendering

---
+++ client/components/LatestRecipes.jsx
@@ -0,0 +1,8 @@
+"use client";
+import { useEffect } from "react";
+import userStore from "@/lib/userStore";
+import RecipeCard from "./RecipeCard";
+
+export default function LatestRecipes() {
+  return <RecipeCard />;
+}
